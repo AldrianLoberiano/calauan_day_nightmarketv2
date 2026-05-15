ae4cab1d81842b824fd3d78350390d6252f38ff7
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:40:32 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -173,3 +173,4 @@ export function StallMap({ stalls, onStallClick, selectedStallId }: StallMapProp
             <div style={{ position:'relative', zIndex:2, padding:'12px 14px' }}>
 
               {/* MARKET SITE */}
+              <div style={{ display:'flex', gap:35, marginBottom:8 }}>
