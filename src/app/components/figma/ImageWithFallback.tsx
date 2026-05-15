7b1e044f5aa67f9831194d9dad005e656bc6d7b3
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:01:30 +0800

    Delete ImageWithFallback.tsx

--- src/app/components/figma/ImageWithFallback.tsx
+++ src/app/components/figma/ImageWithFallback.tsx
@@ -1,4 +1,3 @@
-      className={`inline-block bg-gray-100 text-center align-middle ${className ?? ''}`}
       style={style}
     >
       <div className="flex items-center justify-center w-full h-full">
