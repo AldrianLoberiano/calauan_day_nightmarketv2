421276cf712575c803c4d72a989d80b2eb53d11f
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:25:20 +0800

    Update ReservationFormModal.tsx

--- src/app/components/primitives/ReservationFormModal.tsx
+++ src/app/components/primitives/ReservationFormModal.tsx
@@ -168,3 +168,4 @@ export function ReservationFormModal({ stall, onClose, onSuccess }: ReservationF
             <div className="relative">
               <Building2 className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
               <input
+                type="text"
