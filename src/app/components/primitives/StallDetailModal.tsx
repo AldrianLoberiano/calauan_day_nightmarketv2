e3dbba13dafee35675850ab522ae8ac0675cffc5
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:28:06 +0800

    Update StallDetailModal.tsx

--- src/app/components/primitives/StallDetailModal.tsx
+++ src/app/components/primitives/StallDetailModal.tsx
@@ -2,3 +2,4 @@ import React from 'react';
 import { X, MapPin, Tag, Ruler, ShoppingBag, CheckCircle, Clock, XCircle, MinusCircle } from 'lucide-react';
 import { Stall } from '../../types';
 import { formatPeso, getStatusTextClass, getStatusLabel } from '../../utils/helpers';
+
