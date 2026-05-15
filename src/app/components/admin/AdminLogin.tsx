35258a30a84426f1e7ea22388b9302a8b0fcfee6
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 23:58:28 +0800

    Update AdminLogin.tsx

--- src/app/components/admin/AdminLogin.tsx
+++ src/app/components/admin/AdminLogin.tsx
@@ -46,7 +46,6 @@ export function AdminLogin({ onLoginSuccess }: AdminLoginProps) {
             <img
               src={bploLogo}
               alt="BPLO Logo"
-              className="w-15 h-15 object-contain"
             />
           </div>
           <h1 className="text-white text-2xl font-black tracking-tight">BPLO Admin Panel</h1>
