1caf8aa1812acb9cb9680c3af8b85c266a2dc415
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:05:04 +0800

    Update ReservationFormModal.tsx

--- src/app/components/primitives/ReservationFormModal.tsx
+++ src/app/components/primitives/ReservationFormModal.tsx
@@ -2,3 +2,4 @@ import React, { useState } from 'react';
 import { X, User, Phone, Building2, MapPin, Loader2 } from 'lucide-react';
 import { Stall, Reservation } from '../../types';
 import { formatPeso } from '../../utils/helpers';
+import { addReservation, updateStall, generateReservationNumber, generateUUID } from '../../utils/storage';
