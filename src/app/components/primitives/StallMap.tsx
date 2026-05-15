d806da59d4093c81a501ff657d29411096e3c3a0
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:38:25 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -130,3 +130,4 @@ export function StallMap({ stalls, onStallClick, selectedStallId }: StallMapProp
               viewBox="0 0 100 100"
               preserveAspectRatio="none"
               style={{ position:'absolute', inset:0, width:'100%', height:'100%', pointerEvents:'none', zIndex:1 }}
+              {/* Horizontal roads */}
