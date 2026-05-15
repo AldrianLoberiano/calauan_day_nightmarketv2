91e66a51514310aaa0288286780ad4bf358c488f
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:38:23 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -129,3 +129,4 @@ export function StallMap({ stalls, onStallClick, selectedStallId }: StallMapProp
             <svg
               viewBox="0 0 100 100"
               preserveAspectRatio="none"
+              style={{ position:'absolute', inset:0, width:'100%', height:'100%', pointerEvents:'none', zIndex:1 }}
