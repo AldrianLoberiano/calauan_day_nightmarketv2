63e55a45eb6bed2be9f3bfa16b895773fe214a7d
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 18:14:01 +0800

    Update UserPage.tsx

--- src/app/pages/UserPage.tsx
+++ src/app/pages/UserPage.tsx
@@ -62,6 +62,7 @@ export function UserPage() {
       <header
         className="relative text-white bg-center bg-cover"
         style={{ backgroundImage: `url(${headerImage})` }}
+      >
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-3">
               <div className="w-10 h-10 bg-white/20 rounded-xl flex items-center justify-center">
