99cff2828150617c9cb6a4a0d733a5f144388458
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 19:30:24 +0800

    Update StallMap.tsx

--- src/app/components/StallMap.tsx
+++ src/app/components/StallMap.tsx
@@ -217,6 +217,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId }: StallMapProp
                   <HR ss={inTop} w={20} h={16}/>
                   <div style={{ display:'flex', gap:2, marginLeft:6, marginRight:6 }}>
                     {cD.map(s=><CS key={s.id} s={s}/>)}
+                  </div>
                   <CL t="D"/>
                 </div>
                 <div style={{ width:350, flexShrink:0 }}/>
