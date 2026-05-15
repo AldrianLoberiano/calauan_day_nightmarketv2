7e7ad8ad25074cfd7fb3cf8fff8fa59a11479648
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 18:22:03 +0800

    Update StallMap.tsx

--- src/app/components/StallMap.tsx
+++ src/app/components/StallMap.tsx
@@ -129,6 +129,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId }: StallMapProp
             <svg
               viewBox="0 0 100 100"
               preserveAspectRatio="none"
+              style={{ position:'absolute', inset:0, width:'100%', height:'100%', pointerEvents:'none', zIndex:1 }}
               {/* Horizontal roads */}
               {['17%','17.6%'].map((y,i)=>(
                 <React.Fragment key={`hr${i}`}>
