1d08b77665447b1f26ba54c5d133cfe16a904bab
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:36:45 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -83,3 +83,4 @@ export function StallMap({ stalls, onStallClick, selectedStallId }: StallMapProp
       justifyContent:'center', flexShrink:0, boxShadow:'0 1px 3px rgba(0,0,0,0.2)' }}>{t}</div>
   );
 
+  // Helpers
