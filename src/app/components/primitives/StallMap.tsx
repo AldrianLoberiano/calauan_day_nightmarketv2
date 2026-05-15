b8a56246c7b4519fed038e77aff5e5e4305a4c69
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:33:18 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -3,4 +3,5 @@ import { Stall } from '../../types';
 import { getStallColorClass } from '../../utils/helpers';
 
 interface StallMapProps {
+  stalls: Stall[];
   onStallClick: (stall: Stall) => void;
