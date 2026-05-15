6fa8e6d8c1fd937cb698951dff2e70ae5c887921
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 18:38:55 +0800

    Update UserPage.tsx

--- src/app/pages/UserPage.tsx
+++ src/app/pages/UserPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router';
 import { MapPin, Info, ShieldCheck, Search, ChevronDown } from 'lucide-react';
 import { Stall, Reservation } from '../types';
 import headerImage from '../components/public/header1.png';
