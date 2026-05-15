50c2bc6e17eb92e6c1ecd977f7a68d74fbb715f4
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:41:58 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -212,3 +212,4 @@ export function StallMap({ stalls, onStallClick, selectedStallId }: StallMapProp
               <div style={{ display:'flex', gap:0, marginBottom:2 }}>
                 <div style={{ width:52, flexShrink:0 }}/>
                 <div style={{ display:'flex', alignItems:'center', gap:2, flex:1 }}>
+                  <CL t="C"/>
