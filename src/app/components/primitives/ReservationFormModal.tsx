b1f8b67259afa553409162287c22ff73b7015475
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:24:36 +0800

    Update ReservationFormModal.tsx

--- src/app/components/primitives/ReservationFormModal.tsx
+++ src/app/components/primitives/ReservationFormModal.tsx
@@ -148,3 +148,4 @@ export function ReservationFormModal({ stall, onClose, onSuccess }: ReservationF
             <div className="relative">
               <Phone className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
               <input
+                type="tel"
