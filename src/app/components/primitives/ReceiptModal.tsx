d72333e95c5e69875afcd2b6a53d71152b69e956
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:04:20 +0800

    Update ReceiptModal.tsx

--- src/app/components/primitives/ReceiptModal.tsx
+++ src/app/components/primitives/ReceiptModal.tsx
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react';
 import { X, Printer, CheckCircle, MapPin, Phone, User, Calendar, Building2, Clock } from 'lucide-react';
 import { Reservation, Stall } from '../types';
-import { formatDate, formatPeso } from '../utils/helpers';
 
 interface ReceiptModalProps {
   reservation: Reservation | null;
