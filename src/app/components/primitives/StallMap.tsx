eded6c81804af66a89af86e507cc8535aa014f04
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:40:45 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -179,3 +179,4 @@ export function StallMap({ stalls, onStallClick, selectedStallId }: StallMapProp
                   {/* Pillars */}
                   {range(0,14).map(i=>(
                     <div key={`p${i}`} style={{ position:'absolute', left:0, top:i*10+2, width:10, height:8,
+                      borderRight:'1.5px solid #aaa', background:'#e0e0e0' }}/>
