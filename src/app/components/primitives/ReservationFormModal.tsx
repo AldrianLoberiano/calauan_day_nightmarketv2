76cd6cd7af05f9a8efb8245d1ddb85319117a4bb
AldrianLoberiano <loberianorian@gmail.com>
2026-05-15 21:27:21 +0800

    Update ReservationFormModal.tsx

--- src/app/components/primitives/ReservationFormModal.tsx
+++ src/app/components/primitives/ReservationFormModal.tsx
@@ -220,3 +220,4 @@ export function ReservationFormModal({ stall, onClose, onSuccess }: ReservationF
                   Processing...
                 </>
               ) : (
+                'Confirm Reservation'
