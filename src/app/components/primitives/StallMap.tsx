380c1c30e38ad2a087b0c517c11f8e6a090c88fa
AldrianLoberiano <loberianorian@gmail.com>
2026-05-16 17:19:57 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -213,6 +213,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId }: StallMapProp
                   <CL t="C"/>
                   <div style={{ display:'flex', gap:-8, marginRight:6, alignItems:'flex-end', transform:'rotate(50deg)', transformOrigin:'left bottom' }}>
                     {cC.map((s, idx) => {
+                      const t = idx - (cC.length - 1) / 2;
                   <HR ss={inTop} w={20} h={16}/>
                   <div style={{ display:'flex', gap:-8, marginLeft:6, marginRight:6, alignItems:'flex-end', transform:'rotate(50deg)', transformOrigin:'left bottom' }}>
                     {cD.map((s, idx) => {
