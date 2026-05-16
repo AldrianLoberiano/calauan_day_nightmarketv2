2b8d496c9aaac0cdc36ff78249ef856f7847abc6
AldrianLoberiano <loberianorian@gmail.com>
2026-05-17 07:23:09 +0800

    Update AdminDashboard.tsx

--- src/app/components/admin/AdminDashboard.tsx
+++ src/app/components/admin/AdminDashboard.tsx
@@ -317,7 +317,6 @@ export function AdminDashboard({ onLogout }: AdminDashboardProps) {
                     key={res.id}
                     reservation={res}
                     stall={getStallForReservation(res)}
-                      setExpandedReservationId(prev => (prev === res.id ? null : res.id))
                     }
                     onUpdate={loadData}
                   />
