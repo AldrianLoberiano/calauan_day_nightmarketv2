831b164b0bdfca4fa2ec4da8d5e9b4a853377519
AldrianLoberiano <loberianorian@gmail.com>
2026-05-16 15:11:10 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -185,7 +185,6 @@ export function StallMap({ stalls, onStallClick, selectedStallId }: StallMapProp
                   <div style={{ position:'absolute', inset:9, border:'1.5px solid #bbb' }}/>
                   <div style={{ position:'absolute', inset:16, border:'1px dashed #ccc' }}/>
                   {/* Bottom arch detail */}
-                  <svg style={{ position:'absolute', bottom:5, left:60, width:200, height:25 }}>
                     <path d="M 0,25 Q 50,0 100,25" fill="none" stroke="#bbb" strokeWidth="1"/>
                     <path d="M 100,25 Q 150,0 200,25" fill="none" stroke="#bbb" strokeWidth="1"/>
                   </svg>
