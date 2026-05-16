387825f2198c8708bf646332466301e3f40210dd
AldrianLoberiano <loberianorian@gmail.com>
2026-05-16 15:51:57 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -216,7 +216,6 @@ export function StallMap({ stalls, onStallClick, selectedStallId }: StallMapProp
                   <div style={{ display:'flex', gap:-8, marginLeft:6, marginRight:6, alignItems:'flex-end' }}>
                     {cD.map((s, idx) => {
                       const t = idx - (cD.length - 1) / 2;
-                      const angle = t * 14;
                       const offsetY = 8 + (t * t) * 2.9;
                       const offsetX = 0;
                       return (
