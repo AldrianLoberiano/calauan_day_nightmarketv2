dd0904ad4a4257d28b4a1c08aeca8b46f12bb99f
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 10:16:03 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -323,6 +323,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
                       const offsetX = 0;
                       return (
                         <div
+                          key={s.id}
                   <HR ss={inBot} w={20} h={16}/>
                   <div style={{ display:'flex', gap:2, marginLeft:6, marginRight:6 }}>
                     {cA.map(s=><CS key={s.id} s={s}/>)}
