9270be670e20394013273c5ebac6b5ad7985511b
AldrianLoberiano <loberianorian@gmail.com>
2026-05-18 10:27:43 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -326,6 +326,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
                           key={s.id}
                           style={{
                             transform:`translate(${offsetX}px, ${offsetY}px) rotate(${angle}deg)`,
+                            transformOrigin:'50% 80%'
                           }}
                         >
                           <CS s={s}/>
