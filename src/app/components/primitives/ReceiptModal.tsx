c48bc8387c8abb02e34113c735f5ae1a1c60282b
AldrianLoberiano <loberianorian@gmail.com>
2026-05-19 11:15:13 +0800

    Update ReceiptModal.tsx

--- src/app/components/primitives/ReceiptModal.tsx
+++ src/app/components/primitives/ReceiptModal.tsx
@@ -108,6 +108,7 @@ export function ReceiptModal({ reservation, stall, onClose }: ReceiptModalProps)
               {reservation.businessName && (
                 <DetailRow icon={<Building2 className="w-4 h-4 text-blue-600" />} label="Business Name" value={reservation.businessName} />
               )}
+              {reservation.dtiNumber && (
               <DetailRow
                 icon={<Calendar className="w-4 h-4 text-blue-600" />}
                 label="Reserved On"
