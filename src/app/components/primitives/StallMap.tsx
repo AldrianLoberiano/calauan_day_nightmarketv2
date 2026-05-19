6859fcaf7130051a625e09758ec8e3d62bfa058b
AldrianLoberiano <loberianorian@gmail.com>
2026-05-19 08:36:33 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -344,6 +344,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
                       const offsetY = -6 + (t * t) * 3.2;
                       const offsetX = 0;
                       return (
+                        <div
                           <CS s={s}/>
                         </div>
                       );
