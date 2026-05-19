14ebf6a892895dff97e1aa80f9998f41efa712fe
AldrianLoberiano <loberianorian@gmail.com>
2026-05-19 23:17:04 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -323,6 +323,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
                     {[...cB].reverse().map((s, idx) => {
                       const t = idx - (cB.length - 1) / 2;
                       const angle = -t * -15;
+                      const offsetY = -140 + (t * t) * 3;
                       return (
                         <div
                           key={s.id}
