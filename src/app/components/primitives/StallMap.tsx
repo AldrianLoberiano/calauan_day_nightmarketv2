f0b5ac9a8314a04c579f74a249355e236c762141
AldrianLoberiano <loberianorian@gmail.com>
2026-05-19 09:35:10 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -364,6 +364,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
               {/* BOTTOM OUTER ROWS */}
               <div style={{ display:'flex', gap:30, marginTop:3 }}>
                 <div style={{ marginLeft:30 }}>
+                  <HR ss={outBL} w={17} h={15}/>
               </div>
             </div>
           </div>
