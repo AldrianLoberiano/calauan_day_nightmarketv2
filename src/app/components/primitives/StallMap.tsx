6aa017d5cc9ed2dbfd5f59798e8aba6c8fa85c4d
AldrianLoberiano <loberianorian@gmail.com>
2026-05-20 13:06:44 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -346,6 +346,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
                 </div>
                 <div style={{ display:'flex', alignItems:'center', gap:2, flex:1 }}>
                   <div style={{ display:'flex', gap:-10, marginRight:6, marginTop:-310, alignItems:'flex-end', transform:'rotate(-120deg)', transformOrigin:'right bottom' }}>
+                  <div style={{ display:'flex', gap:-10, marginRight:25, marginTop:-305, alignItems:'flex-end', transform:'rotate(-120deg)', transformOrigin:'right bottom' }}>
                     {[...cB].reverse().map((s, idx) => {
                       const t = idx - (cB.length - 1) / 2;
                       const angle = -t * -15;
