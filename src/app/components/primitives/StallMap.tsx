fc48f6e075c687f234bbba247d150a1978a3bd75
AldrianLoberiano <loberianorian@gmail.com>
2026-05-20 10:54:07 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -257,6 +257,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
                   <div style={{ height:28, marginBottom:6 }} />
                   <div style={{ position:'absolute', left:0, top:'50%', transform:'translateY(-50%)' }}>
                   </div>
+                  </div>
                   {range(0, pairs-1).map(i=>(
                     <div key={`lp${i}`} style={{ display:'flex', gap:10 }}>
                       <S s={lOut[i]} w={25} h={18}/>
