8b9a36a93b0caf1184be6a82591db8bb38f20af9
Vienna Mendoza <loberianorian@gmail.com>
2026-05-21 16:46:14 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -376,6 +376,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
                       {Array.from({ length: leftColumnRows }, (_, idx) => {
                         const slot = bbColumnSlots[idx];
                         return slot ? <S key={slot.stall?.id ?? `bb-${idx}`} slot={slot} w={25} h={18}/> : <div key={`bb-empty-${idx}`} style={{ width:25, height:18 }} />;
+                      })}
                 </div>
 
                 {/* Center + right col */}
