e18f952afd47745ee45ae9eb8a52337b9981d5d1
Vienna Mendoza <loberianorian@gmail.com>
2026-05-24 07:13:03 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -453,7 +453,6 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
                   <div style={{ marginLeft:-68 }}>
                     <HR ss={inBot} w={20} h={16}/>
                   </div>
-                  <div style={{ display:'flex', gap:-16, marginLeft:-10, marginRight:0, marginTop:-310, alignItems:'flex-end', transform:'rotate(135deg)', transformOrigin:'left bottom' }}>
                     {cA.map((s, idx) => {
                       const t = idx - (cA.length - 1) / 2;
                       const offsetY = -130 + (t * t) * 4;  
