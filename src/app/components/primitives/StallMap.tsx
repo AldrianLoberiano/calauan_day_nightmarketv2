f3d1d051dabd710c0029bd4c1d71065cd8c75a1c
Vienna Mendoza <loberianorian@gmail.com>
2026-05-24 10:12:12 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -282,6 +282,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
 
               {/* TOP OUTER ROW (C1–C37) */}
               <div style={{ display:'flex', gap:8, marginBottom:2, alignItems:'center', marginLeft:120 }}>
+                <CL t="C" />
               {/* INNER TOP ROW (with C/D) */}
               <div style={{ display:'flex', gap:0, marginBottom:2, position:'relative', zIndex:5 }}>
                 <div style={{ width:52, flexShrink:0 }}/>
