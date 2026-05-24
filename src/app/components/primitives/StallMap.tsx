e76bfa5feb61c34b25c892a542db1f35fdfdd036
Vienna Mendoza <loberianorian@gmail.com>
2026-05-24 10:08:57 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -280,7 +280,6 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
                   fontSize:20, fontWeight:'bold', color:'#555', background:'#fff' }}>5</div>
               </div>
 
-                    ))}
                   </div>
                 </div>
                 <div style={{ marginLeft:10, transform:'translateX(110px)' }}>
