edc28477b0e0356a0ce51231e3364f9484eadc23
Vienna Mendoza <loberianorian@gmail.com>
2026-05-25 11:18:19 +0800

    Update StallDetailModal.tsx

--- src/app/components/primitives/StallDetailModal.tsx
+++ src/app/components/primitives/StallDetailModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { X, MapPin, Tag, Ruler, ShoppingBag, CheckCircle, Clock, XCircle, MinusCircle } from 'lucide-react';
 import { Stall } from '../../types';
+import { formatPeso, getStatusTextClass, getStatusLabel, getDisplayStallId, getDisplaySectionById } from '../../utils/helpers';
 
 interface StallDetailModalProps {
   stall: Stall | null;
