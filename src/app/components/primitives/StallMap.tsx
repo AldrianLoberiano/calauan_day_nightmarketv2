bef65b3861a04f5b06b94347b7a043de9dda9277
Vienna Mendoza <loberianorian@gmail.com>
2026-05-25 11:18:34 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -30,6 +30,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
       const id = startId + i;
       const stall = idMap.get(id);
       if (stall) usedIds.add(id);
+      const label = `${prefix}${i + 1}`;
       slots.push({ stall, label, disabled: !stall });
     }
     return slots;
