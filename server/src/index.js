061ccc1c118bf6f81cb02944e06d166c6069cfed
Vienna Mendoza <loberianorian@gmail.com>
2026-05-25 11:17:28 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -151,6 +151,7 @@ async function nextReservationNumberBySection(connection, section) {
 
 function normalizeReservationSection(stallId, section) {
   const id = (stallId || '').toString().toUpperCase();
+  if (/^\d+$/.test(id)) {
 async function legacyNextReservationNumber(connection, yearOverride) {
   const year = yearOverride ?? new Date().getFullYear();
   // Behave like the previous single-row counter implementation (id=1)
