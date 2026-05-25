712baa1a53ad23027ec9e225817bfcb3c2bb9896
Vienna Mendoza <loberianorian@gmail.com>
2026-05-25 11:20:13 +0800

    Update helpers.ts

--- src/app/utils/helpers.ts
+++ src/app/utils/helpers.ts
@@ -82,6 +82,7 @@ export function getDisplayStallId(stallId: string): string {
   if (stallId === 'A2') return 'G2';
   if (stallId === 'A3') return 'G3';
   if (stallId === 'A4') return 'G4';
+  if (stallId === 'A5') return 'G5';
   if (stallId === 'C1') return 'BB31';
   if (stallId === 'C2') return 'BB32';
   if (stallId === 'C3') return 'BB33';
