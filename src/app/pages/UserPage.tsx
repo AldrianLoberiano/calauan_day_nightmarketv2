0cf7666892d9039c9b64378a1c10464bcfffbeef
Vienna Mendoza <loberianorian@gmail.com>
2026-05-25 11:19:05 +0800

    Update UserPage.tsx

--- src/app/pages/UserPage.tsx
+++ src/app/pages/UserPage.tsx
@@ -59,6 +59,7 @@ export function UserPage() {
     { label: 'B', start: 48, end: 91, displayStart: 1 },
     { label: 'AA', start: 92, end: 133, displayStart: 1 },
     { label: 'BB', start: 134, end: 167, displayStart: 1 },
+    { label: 'C', start: 168, end: 204, displayStart: 1 },
   ];
   const directoryRanges: Record<string, string> = {
     A: '1-47',
