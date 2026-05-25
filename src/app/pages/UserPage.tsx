7cb5dcb2896a649972b4d39b5c2089224b254518
Vienna Mendoza <loberianorian@gmail.com>
2026-05-25 14:32:02 +0800

    Update UserPage.tsx

--- src/app/pages/UserPage.tsx
+++ src/app/pages/UserPage.tsx
@@ -43,7 +43,6 @@ export function UserPage() {
     };
 
     const interval = setInterval(() => setNow(new Date()), 1000);
-    return () => clearInterval(interval);
   }, []);
 
   const safeStalls = Array.isArray(stalls) ? stalls : [];
