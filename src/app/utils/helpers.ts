512c29a01686746daead4688740cb19e1b361db1
Vienna Mendoza <loberianorian@gmail.com>
2026-05-25 11:20:06 +0800

    Update helpers.ts

--- src/app/utils/helpers.ts
+++ src/app/utils/helpers.ts
@@ -79,6 +79,7 @@ export function capitalize(str: string): string {
 export function getDisplayStallId(stallId: string): string {
   if (!stallId) return '';
   if (stallId === 'A1') return 'G1';
+  if (stallId === 'A2') return 'G2';
   if (stallId === 'C1') return 'BB31';
   if (stallId === 'C2') return 'BB32';
   if (stallId === 'C3') return 'BB33';
