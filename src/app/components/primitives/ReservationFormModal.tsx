e62effc29304ded0a7561a619af740e1a0a03d18
Vienna Mendoza <loberianorian@gmail.com>
2026-05-26 09:31:24 +0800

    Update ReservationFormModal.tsx

--- src/app/components/primitives/ReservationFormModal.tsx
+++ src/app/components/primitives/ReservationFormModal.tsx
@@ -208,7 +208,6 @@ export function ReservationFormModal({ stall, onClose, onSuccess }: ReservationF
             />
           </FormField>
 
-            />
           </FormField>
 
           {/* Address */}
