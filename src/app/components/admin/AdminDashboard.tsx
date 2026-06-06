ba07fd76664b33a8416c68f23996f7301e88fc5c
AldrianLoberiano <loberianorian@gmail.com>
2026-06-06 11:41:52 +0800

    Update AdminDashboard.tsx

--- src/app/components/admin/AdminDashboard.tsx
+++ src/app/components/admin/AdminDashboard.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useMemo } from 'react';
 import {
   Search, RefreshCw, LogOut, LayoutDashboard, ClipboardList,
   MapPin, CheckCircle, Clock, XCircle, Package,
-  TrendingUp, Users, Activity
 } from 'lucide-react';
 import { Reservation, Stall } from '../../types';
 import {
