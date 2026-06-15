da3fdbce952506a0d4f26214b5dc4b569f41300a
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-15 11:15:47 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -246,7 +246,6 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
           <div style={{ position:'absolute', inset:0, opacity:0.035, pointerEvents:'none',
             backgroundImage:'repeating-linear-gradient(0deg,#1a1a6e 0,#1a1a6e 1px,transparent 1px,transparent 30px),repeating-linear-gradient(90deg,#1a1a6e 0,#1a1a6e 1px,transparent 1px,transparent 30px)' }}/>
 
-          {/* Outer border */}
           <div style={{ position:'relative', border:'3px solid #1a1a6e' }}>
 
             {/* Road SVG */}
