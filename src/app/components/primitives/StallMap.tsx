e4d753aa59294661e0308603e5eae1d7177fac19
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-15 11:16:28 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -394,6 +394,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
               </div>
 
               {/* MAIN BODY */}
+              <div style={{ display:'flex', gap:0, position:'relative', overflow:'visible' }}>
 
                 {/* Left paired columns */}
                 <div style={{ flexShrink:0, marginLeft:0, marginRight:3, marginTop:25, display:'flex', flexDirection:'column', gap:0, position:'relative' }}>
