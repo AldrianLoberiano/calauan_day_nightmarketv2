e2b5bdd366d4a477c084e44f36b113a6b64b3b7f
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-15 11:15:36 +0800

    Update StallMap.tsx

--- src/app/components/primitives/StallMap.tsx
+++ src/app/components/primitives/StallMap.tsx
@@ -238,6 +238,7 @@ export function StallMap({ stalls, onStallClick, selectedStallId, initialZoom, m
       </div>
 
       <div ref={containerRef} className="w-full overflow-auto" style={{ maxHeight: maxHeight ?? '80vh', overflow:'auto' }}>
+        <div style={{ transform:`scale(${zoom})`, transformOrigin:'top left', minWidth:1850,
           padding:14, background:'#fff', fontFamily:"'Courier New',monospace", position:'relative',
           width:'fit-content' }}>
 
