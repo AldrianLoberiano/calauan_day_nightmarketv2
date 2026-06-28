cd4f57c98f484b46d5d203a5bb6ee17060fc3ade
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-28 21:06:53 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1248,6 +1248,7 @@ app.get('/api/admin/vendors/:id/reservation-count', authAdmin, async (req, res, 
       count += dm[0]?.cnt || 0;
     } catch {}
     try {
+      const [as_] = await pool.query('SELECT COUNT(*) AS cnt FROM all_stalls_reservations WHERE vendor_id = ?', [req.params.id]);
     res.json({ count });
   } catch (err) {
     next(err);
