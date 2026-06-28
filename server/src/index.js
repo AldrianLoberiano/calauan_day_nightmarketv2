ba718bb23cc27de4d6fa488e1807dccbe126eaf0
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-28 21:05:37 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1194,6 +1194,7 @@ app.get('/api/admin/vendors', authAdmin, async (req, res, next) => {
         businessName: r.business_name,
         email: r.email,
         passcode: null,
+        event: null,
   }
 });
 
