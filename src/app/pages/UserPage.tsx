9af0ef2da749f0943d6a6bb069a8c4e97ebb9717
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-28 14:24:37 +0800

    Update UserPage.tsx

--- src/app/pages/UserPage.tsx
+++ src/app/pages/UserPage.tsx
@@ -218,7 +218,6 @@ export function UserPage() {
         <div className="absolute inset-0 bg-slate-900/30 pointer-events-none" />
         <div className="relative max-w-8xl mx-auto px-3 sm:px-5 py-2.5 sm:py-3">
           <div className="flex items-center justify-between">
-                <img src={bploLogo} alt="BPLO Logo" className="w-9 h-9 object-contain" />
               </div>
               <div>
                 <h1 className="font-black text-base sm:text-lg leading-tight tracking-tight">Stall Reservation System</h1>
